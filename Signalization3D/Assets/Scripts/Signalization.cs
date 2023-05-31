8b6a242158a021d42e44712c3b5ea87276e44ec2
Artem <artem.ulm@gmail.com>
2023-05-31 13:58:04 +0300

    Rewrite coroutine

--- Signalization3D/Assets/Scripts/Signalization.cs
+++ Signalization3D/Assets/Scripts/Signalization.cs
@@ -7,7 +7,7 @@ using UnityEngine.Events;
 [RequireComponent(typeof(AudioSource))]
 public class Signalization : MonoBehaviour
 {
-    [SerializeField] private float _volumeChangeDuration;
+    [SerializeField] private float _volumeChangeSpeed;
 
     private SignalizationZone _signalizationZone;
     private AudioSource _audioSource;
@@ -27,58 +27,44 @@ public class Signalization : MonoBehaviour
         _signalizationZone.Alarmed += OnAlarmed;
     }
 
-    private void OnDisable()
+    private void OnDestroy()
     {
         _signalizationZone.Alarmed -= OnAlarmed;
     }
 
     private void OnAlarmed(bool isIntruderInHouse)
     {
+        float desiredVolume;
+
         if (_currentCoroutine != null)
         {
             StopCoroutine(_currentCoroutine);
         }
-
-        _currentCoroutine = StartCoroutine(ControlSignalization(_volumeChangeDuration, isIntruderInHouse));
-    }
-
-    private IEnumerator ControlSignalization(float duration, bool isIntruderInHouse)
-    {
-        float stepsCount = 10f;
-        float proportion;
-        var waitForSeconds = new WaitForSeconds(duration / stepsCount);
-
-        if (isIntruderInHouse)
-        {
-            _maxVolume = 1;
-            _minVolume = _audioSource.volume;
-            _audioSource.Play();
-            stepsCount -= _audioSource.volume * stepsCount;
-        }
         else
         {
-            _minVolume = 0;
-            _maxVolume = _audioSource.volume;
-            stepsCount -= (stepsCount - _audioSource.volume * stepsCount);
-        }
-
-        for (int i = 1; i <= stepsCount; i++)
-        {
-            proportion = i / stepsCount;
-
             if (isIntruderInHouse)
-                _audioSource.volume = Mathf.Lerp(_minVolume, _maxVolume, proportion);
-            else
-                _audioSource.volume = Mathf.Lerp(_maxVolume, _minVolume, proportion);
-
-            yield return waitForSeconds;
+                _audioSource.Play();
         }
 
-        if (isIntruderInHouse == false)
+        if (isIntruderInHouse)
+            desiredVolume = _maxVolume;
+        else
+            desiredVolume = _minVolume;
+
+        _currentCoroutine = StartCoroutine(ControlSignalization(desiredVolume));
+    }
+
+    private IEnumerator ControlSignalization(float desiredVolume)
+    {
+        while (_audioSource.volume != desiredVolume)
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, desiredVolume, _volumeChangeSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        if(_audioSource.volume == _minVolume)
             _audioSource.Stop();
 
-        _minVolume = 0;
-        _maxVolume = 1;
         _currentCoroutine = null;
     }
 }
